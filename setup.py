0f1caec2495d3d2159a28a6d45fabd8a2b85102e
tomasz <tomasz@coinuma.com>
2020-04-22 19:56:22 +0200

    Corrected long description markdown issue

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ NAME = "coinuma-sdk"
 VERSION = "0.2.1"
 DESCRIPTION = "A python SDK for the Coinuma Exchange API"
 LONG_DESCRIPTION = """
-    Welcome to the Coinuma API! Through our cryptocurrency exchange we offer a wide selection 
-    of currencies for you to trade. For a complete API request and response reference please check 
-    out our [API documentation](https://coinuma.com/developers/docs). If you need additional help in using 
-    the API, please visit the [Coinuma website](https://coinuma.com) and reach our support center. 
-    Happy trading!
-    """
+# Welcome to the Coinuma API SDK
+Through our cryptocurrency exchange we offer a wide selection of currencies for you to trade. For a complete API request and response reference please check out our [API documentation](https://coinuma.com/developers/docs). If you need additional help in using the API, please visit the [Coinuma website](https://coinuma.com) and reach our support center. 
+#### Happy trading!
+"""
 URL = "https://coinuma.com"
 URL_DOWNLOAD = "https://github.com/Coinuma/coinuma-sdk-python.git"
 REQUIRES = ["requests >= 2.21.0", "python-dateutil"]
@@ -19,7 +17,7 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    long_description_content="text/plain",
+    long_description_content="text/markdown",
     author_email="info@coinuma.com",
     url=URL,
     download_url=URL_DOWNLOAD,
